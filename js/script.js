6f2c2a5eac8299300c1828b2fc34412befdaa37a
Maria Belan-Crawford <44656740+mViolet@users.noreply.github.com>
2021-01-30 13:07:56 -0600

    added embedded video url support

--- js/script.js
+++ js/script.js
@@ -3,31 +3,29 @@ const text = document.querySelector('#text')
 const header = document.querySelector('h1')
 const inputDate = document.querySelector('input')
 const button = document.querySelector('button')
-const video = document.querySelector('#video')
+const iframe = document.querySelector('iframe')
 const date = new Date()
-const maxDate = date.toJSON().slice(0,10) //get the current date to set the maximum date value
+const maxDate = date.toJSON().slice(0,10) //get the current date as a string to set the maximum date value
 
 inputDate["max"] = maxDate
 
 inputDate.addEventListener('change', setImage)
-// inputDate.addEventListener('change', logEvent)
-
-function logEvent(){
-	// console.log(inputDate.value)
-}
 
 function setImage(){
 	fetch(`https://api.nasa.gov/planetary/apod?date=${inputDate.value}&api_key=fktZNPFrExJcH19jEStMyRclbN3Aa19vKvzkjpFI`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-
-    	if (data.url.includes('youtube')){
-    		text.innerText = "This one's invalid. Try another date"
-    		return
-    	}
-
-    	// console.log(data)
-    	img.src = data.url
+        console.log(data)
+        header.classList.remove('hide')
+    	if (data.url.includes('embed')){
+            img.classList.add('hide')
+            iframe.classList.remove('hide')
+            iframe.src = data.url
+        } else {
+            iframe.classList.add('hide')
+            img.classList.remove('hide')
+            img.src = data.url
+        }
     	header.innerText = data.title
       	text.innerText = data.explanation
     })
